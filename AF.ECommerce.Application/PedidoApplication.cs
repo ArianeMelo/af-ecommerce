79bb4e6bc4e795ff0b685167e0f3522cec770881
Ariane Melo <ariane.admquirino@gmail.com>
2023-02-10 10:04:47 -0300

    Inclusao UnityOfWork PedidoService

--- AF.ECommerce.Application/PedidoApplication.cs
+++ AF.ECommerce.Application/PedidoApplication.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation.Results;
+using Newtonsoft.Json;
 
 namespace AF.ECommerce.Application
 {
@@ -21,12 +22,14 @@ namespace AF.ECommerce.Application
         private readonly IPedidoItemRepository _pedidoItemRepository;
         private readonly ILogger<PedidoApplication> _logger;
         private ValidationResult _validationResult;
+        private readonly IUnityOfWork _unityOfWork;
 
         public PedidoApplication(
             IPedidoRepository pedidoRepository,
             IProdutoRepository produtoRepository,
             IPedidoItemRepository pedidoItemRepository,
-            ILogger<PedidoApplication> logger)
+            ILogger<PedidoApplication> logger,  
+            IUnityOfWork unityOfWork)
 
         {
             _pedidoRepository = pedidoRepository;
@@ -34,6 +37,7 @@ namespace AF.ECommerce.Application
             _pedidoItemRepository = pedidoItemRepository;
             _logger = logger; 
             _validationResult = new ValidationResult();
+            _unityOfWork = unityOfWork;
         }
 
         public async Task<Pedido> ObterPorId(Guid id)
@@ -54,43 +58,71 @@ namespace AF.ECommerce.Application
 
         public async Task<bool> AdicionarPedido(Pedido pedido)
         {
-            List<Produto> listaProdutoAInserir = new List<Produto>();           
+            try
+            {       
+                List<Produto> listaProdutoAInserir = new List<Produto>();
 
-            foreach (var item in pedido.Itens)
-            {
-                var produto = await _produtoRepository.ObterPorId(item.ProdutoId);
-                if (produto == null)
-                    return false;
+                foreach (var item in pedido.Itens)               {
+                   
 
-                var possoDebitar = produto.VerificarSePodeDebitarEstoque(item.Quantidade);
+                    var produto = await _produtoRepository.ObterPorId(item.ProdutoId);
+                    if (produto == null)
+                        return false;
 
-                  int QUANTIDADE_PERMITIDA_POR_VENDA = 11;
-                
+                    var possoDebitar = produto.VerificarSePodeDebitarEstoque(item.Quantidade);
 
-                if (possoDebitar && item.Quantidade < QUANTIDADE_PERMITIDA_POR_VENDA) //true se puder debitar 
-                    listaProdutoAInserir.Add(produto);
+                    int QUANTIDADE_PERMITIDA_POR_VENDA = 11;
 
-                else
-                    return false;
+
+                    if (possoDebitar && item.Quantidade < QUANTIDADE_PERMITIDA_POR_VENDA)  //true se puder debitar 
+                        listaProdutoAInserir.Add(produto);
+
+                    else
+                        return false;
+                }
+
+                _unityOfWork.IniciarTransacao();
+                _pedidoRepository.AtribuirTransacao(_unityOfWork);
+                _pedidoItemRepository.AtribuirTransacao(_unityOfWork);
+                _produtoRepository.AtribuirTransacao(_unityOfWork);
+
+                await _pedidoRepository.Adicionar(pedido);
+
+                var logPedido = JsonConvert.SerializeObject(pedido, Formatting.Indented);
+                _logger.LogInformation($"Adicionando pedido : {logPedido}.");
+
+                foreach (var item in pedido.Itens)
+                {
+                    var pedidoItem = new PedidoItem(item.PedidoId, item.ProdutoId, item.Quantidade, item.Valor, item.Desconto)
+                    {
+                        Id = item.Id
+                    };
+
+                    await _pedidoItemRepository.Adicionar(pedidoItem);
+
+                    var logItensPedido = JsonConvert.SerializeObject(pedidoItem, Formatting.Indented);
+                    _logger.LogInformation($"Adicionando itens do pedido : {logItensPedido}.");
+
+
+                    var produto = listaProdutoAInserir.FirstOrDefault(prod => prod.Id == item.ProdutoId);
+                    produto.DebitarEstoque(item.Quantidade);
+                    await _produtoRepository.Alterar(produto);
+
+                    var logProduto = JsonConvert.SerializeObject(produto, Formatting.Indented);
+                    _logger.LogInformation($"Alterando estoque do produto : {logProduto}.");
+                }
+
+                _unityOfWork.CommittarTransacao();
+
+                _logger.LogInformation("Commit Realizado");
+
                
             }
-
-            await _pedidoRepository.Adicionar(pedido);
-
-            foreach (var item in pedido.Itens)
+            catch (Exception)
             {
-                var pedidoItem = new PedidoItem(item.PedidoId, item.ProdutoId, item.Quantidade, item.Valor, item.Desconto)
-                {
-                    Id = item.Id
-                };
-
-                await _pedidoItemRepository.Adicionar(pedidoItem);
-
-                
-                var produto = listaProdutoAInserir.FirstOrDefault(prod => prod.Id == item.ProdutoId);
-                produto.DebitarEstoque(item.Quantidade);
-                await _produtoRepository.Alterar(produto);
-
+                _unityOfWork.RollbackTransacao();
+                _logger.LogInformation("Rollback Realizado");
+                throw;
             }
 
             return true;
